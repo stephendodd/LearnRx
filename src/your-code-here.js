9b59bbe64972543dfb49abfe6be0992b1b90245d
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 17:52:22 +0800

    No indexers

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,17 @@
-function exercise (console) {
-
+/*
+ Exercise 12: No index
+    Try to retrieve {name, identity, avatar} for every heroes.
+    We only want the avatar with size 100*100.
+    AND YOU CANNOT USE INDEXERS, superHeroes[0] IS ILLEGAL! I will tell you why in the later exercises.
+ */
+function exercise (superHeroes) {
+    let heroesWithAvatar;
+    /*
+     Add your code here, use one or more contactAll, map, filter to project on the tree.
+     Try to give me heroes like this:
+     [{name:'Superman', identity: 'Clerk Kent', avatar: 'http://super-heroes.com/superman/medium.jpg'}, ...]
+     */
+    return heroesWithAvatar;
 }
 
 export default exercise;
