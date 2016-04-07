685e2d186e4263b06ac179e96c02849485eb142a
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 15:50:37 +0800

    use map function

--- src/data.js
+++ src/data.js
@@ -1,1 +1,18 @@
-export default [];
+const superHeroes = [{
+    id: 10001,
+    name: 'Superman',
+    identity: 'Clerk Kent',
+    superPower: ['Flight', 'Strength', 'X-Ray vision']
+}, {
+    id: 10002,
+    name: 'Batman',
+    identity: 'Bruce Wayne',
+    superPower: ['Rich', 'Martial arts', 'Espionage']
+}, {
+    id: 10003,
+    name: 'Wonder Woman',
+    identity: 'Diana Prince',
+    superPower: ['Flight', 'Martial arts', 'God-forged Weapons']
+}];
+
+export default [superHeroes];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,17 @@
-function exercise (console) {
+/*
+ Exercise 5: User map to project an Array
+    Select name and identity from the superHeroes array
+ */
+function exercise (superHeroes) {
+    let heroWithIdentity;
 
+    /*
+     Add your code here,
+     use Array.prototype.map function you just implemented to select {name, identity} pairs from superHeroes,
+     and assign the result to heroWithIdentity;
+     */
+
+    return heroWithIdentity;
 }
 
 export default exercise;
