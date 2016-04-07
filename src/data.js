ddaad0e5ca10986d116cccbbb8582bc92e89aae6
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 17:35:54 +0800

    use contactAll and map

--- src/data.js
+++ src/data.js
@@ -1,1 +1,35 @@
-export default [];
+const superHeroes = [
+    {
+        world: 'DC',
+        heroes: [
+            {
+                id: 10001,
+                name: 'Superman',
+                identity: 'Clerk Kent',
+                superPower: ['Flight', 'Strength', 'X-Ray vision']
+            }, {
+                id: 10002,
+                name: 'Batman',
+                identity: 'Bruce Wayne',
+                superPower: ['Rich', 'Martial arts', 'Espionage']
+            }
+        ]
+    }, {
+        world: 'MARVEL',
+        heroes: [
+            {
+                id: 20001,
+                name: 'Thor',
+                identity: 'Thor',
+                superPower: ['Flight', 'Strength', 'Lightning']
+            }, {
+                id: 20002,
+                name: 'Ironman',
+                identity: 'Tony Stark',
+                superPower: ['Rich', 'Flight', 'Missile', 'Laser']
+            }
+        ]
+    }
+];
+
+export default [superHeroes];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,12 @@
-function exercise (console) {
-
+/*
+ Exercise 11: Use map and contactAll to project and flatten then heroes from 2 worlds to name array
+ */
+function exercise (superHeroes) {
+    let allHeroNames;
+    /*
+     Add your code here
+     */
+    return allHeroNames;
 }
 
 export default exercise;
