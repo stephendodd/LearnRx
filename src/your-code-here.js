8bf631919f21078f33764993bcda4267c8feafb7
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 15:23:44 +0800

    forEach exercise

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,11 @@
-function exercise (console) {
-
+/*
+ Exercise 2: Use forEach to loop the Array
+    Print all names in superHeroes array by using forEach
+ */
+function exercise (superHeroes, console) {
+    /*
+        Add your code here and try to use Array.prototype.forEach
+     */
 }
 
 export default exercise;
