44543aea67be7724a198702aa02b4302d49709df
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 16:13:57 +0800

    implement filter

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
 
 function postCheck() {
-
+    var afterFilter = [-1,0,1].filter(x => x >= 0);
+    return `[-1,0,1].filter(x => x >= 0) is [${afterFilter}]`;
 }
 
 export default postCheck;
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,21 @@
-function exercise (console) {
+/*
+ Exercise 7: Implement filter
+    To make filtering easier, add a filter function.
+    The filter function accept a predicate, which is a function that accepts an item,
+     and says if the item should be included in the new array.
 
+    Example: [-1,0,1].filter(x => x >= 0) to be [0, 1]
+ */
+function exercise () {
+    Array.prototype.filter = function(predicate) {
+        let sourceArray = this,
+            results = [];
+
+        /*
+         Add your code here
+         */
+        return results;
+    }
 }
 
 export default exercise;
