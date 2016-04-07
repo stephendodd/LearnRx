ba69275206bfd6d066fb52ad4fe9e385b7913ae5
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 17:18:54 +0800

    forEach to flatten a tree

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,13 @@
-function exercise (console) {
-
+/*
+ Exercise 9: Flatten a Tree
+    Using 2 nested forEach loops to collect the hero names from the 2 worlds.
+ */
+function exercise (superHeroes) {
+    let allHeroNames = [];
+    /*
+     Add your code here
+     */
+    return allHeroNames;
 }
 
 export default exercise;
