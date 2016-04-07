4c96fe3ee12fa5fbe52b8217c37bf90de19e8a49
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 15:37:01 +0800

    project array by forEach

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,16 @@
-function exercise (console) {
+/*
+ Exercise 3: Project on an Array
+    Select name and identity from the superHero array
+ */
+function exercise (superHeroes) {
+    let heroWithIdentity = [];
 
+    /*
+    Add your code here, use forEach function to accumulate {name, identity} pairs from superHero
+    Push the results into heroWithIdentity. Examples: heroWithIdentity.push(newHero);
+     */
+
+    return heroWithIdentity;
 }
 
 export default exercise;
