415547240cc98fc26e7b4ce6c2c5374269ba0978
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 16:05:00 +0800

    filtering array

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,18 @@
-function exercise (console) {
+/*
+ Exercise 6: Filtering Array
+    Filtering elements from an array is a very common operation.
+    To Filter an array, we apply a test to each item in the array,
+     and collect items which pass the test into a new array.
+ */
+function exercise (superHeroes) {
+    let flyableHero = [];
+    /*
+     Use forEach function to select every hero with the power Flight.
+     Array.prototype.includes can return true if the array contains the item.
+     Example: ['a','b','c'].includes('b') is true
+     */
 
+    return flyableHero;
 }
 
 export default exercise;
