3e2511d4ea64d92f1086100fa587e5747e6b7e98
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 16:28:35 +0800

    chain filter and map

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,15 @@
-function exercise (console) {
+/*
+ Exercise 8: Query Data by Chaining Method Calls
+    Chain filter and map to collect the {name, identity} pairs of hero that have Power Martial arts
+ */
+function exercise (superHeroes) {
+    let martialArtsMaster;
 
+    /*
+     Add your code here, chain the filter and map function
+     */
+
+    return martialArtsMaster;
 }
 
 export default exercise;
