046e57f4901fed670e35b60c90fa484e137d91da
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 15:46:31 +0800

    implement map function

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,9 @@
 import _ from 'lodash'
 
 function postCheck() {
-
+    var source = [1,2,3];
+    var afterMap = [1,2,3].map(x => x + 1);
+    return `[${source}].map(x => x + 1) is [${afterMap}]`;
 }
 
 export default postCheck;
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,21 @@
-function exercise (console) {
+/*
+ Exercise 4: Implement map
+    Implement map function to make projection easier.
+    Map accept a projection function to be applied to each of the item in the array, and returns the projected array.
 
+    Example: [1,2,3].map(x => x + 1) to be [2,3,4]
+ */
+function exercise () {
+    Array.prototype.map = function(projectionFunction) {
+        let sourceArray = this,
+            results = [];
+
+        /*
+         Add your code here
+         */
+
+        return results;
+    }
 }
 
 export default exercise;
