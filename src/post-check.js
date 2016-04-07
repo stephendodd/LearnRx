d2e8b5b8563e19c9f7243849759a8bccaef2e0b0
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 17:27:13 +0800

    implement contactAll

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
 
 function postCheck() {
-
+    var afterContactAll = [[1,2], [3,4], [5,6]].contactAll();
+    return `[[1,2],[3,4],[5,6]].contactAll() is [${afterContactAll}]`;
 }
 
 export default postCheck;
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,22 @@
-function exercise (console) {
+/*
+ Exercise 10: Implement contactAll
+    contactAll function iterates over each sub-array in the array,
+    and collect the results in a new, flat array.
 
+    Example: [[1,2], [3,4], [5,6]].contactAll() is [1,2,3,4,5,6]
+    * This function is also known as flatten or flattenDeep in other libs.
+ */
+function exercise () {
+    Array.prototype.contactAll = function() {
+        let sourceArray = this,
+            results = [];
+
+        /*
+         Add your code here, and add all the items in each subArray to the results array.
+         */
+
+        return results;
+    }
 }
 
 export default exercise;
