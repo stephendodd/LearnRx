dd7b3a96ef2bd0dce51f9d4db05c54bec0590405
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 15:09:54 +0800

    Implement contactMap

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
 
 function postCheck() {
-
+    let afterContactMap = [1, 2].contactMap(x => [x, x]);
+    return `[1,2].contactMap(x => [x, x]) is [${afterContactMap}]`;
 }
 
 export default postCheck;
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,22 @@
-function exercise (console) {
+/*
+ Exercise 13: Implement contactMap
+    map and contactAll are very commonly chained together.
+    [1,2].contactMap(x => [x, x]]) is [1,1,2,2]
 
+
+    This contactMap function is also known as flatMap
+ */
+function exercise () {
+    Array.prototype.contactMap = function(projectionFunctionReturnArray) {
+        let sourceArray = this,
+            result = [];
+
+        /*
+         Add your code here
+         */
+
+        return result;
+    }
 }
 
 export default exercise;
