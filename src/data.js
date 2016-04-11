6ad193ed425ba2927dac50eef4b8519859629392
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 15:39:37 +0800

    use contactMap

--- src/data.js
+++ src/data.js
@@ -1,1 +1,51 @@
-export default [];
+const superHeroes = [
+    {
+        world: 'DC',
+        heroes: [
+            {
+                id: 10001,
+                name: 'Superman',
+                identity: 'Clerk Kent',
+                superPower: ['Flight', 'Strength', 'X-Ray vision'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/superman/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/superman/large.jpg'}
+                ]
+            }, {
+                id: 10002,
+                name: 'Batman',
+                identity: 'Bruce Wayne',
+                superPower: ['Rich', 'Martial arts', 'Espionage'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/batman/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/batman/large.jpg'}
+                ]
+            }
+        ]
+    }, {
+        world: 'MARVEL',
+        heroes: [
+            {
+                id: 20001,
+                name: 'Thor',
+                identity: 'Thor',
+                superPower: ['Flight', 'Strength', 'Lightning'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/thor/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/thor/large.jpg'}
+                ]
+            }, {
+                id: 20002,
+                name: 'Ironman',
+                identity: 'Tony Stark',
+                superPower: ['Rich', 'Flight', 'Missile', 'Laser'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/ironman/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/ironman/large.jpg'}
+                ]
+            }
+        ]
+    }
+];
+
+export default [superHeroes];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,16 @@
-function exercise (console) {
-
+/*
+ Exercise 14: Use contactMap!
+ Try to retrieve {name, identity, avatar} for every heroes.
+ We only want the avatar with size 100*100.
+ */
+function exercise (superHeroes) {
+    let heroesWithAvatar;
+    /*
+     Add your code here, use one or more contactMap, map, filter to project on the tree.
+     Try to give me heroes like this:
+     [{name:'Superman', identity: 'Clerk Kent', avatar: 'http://super-heroes.com/superman/medium.jpg'}, ...]
+     */
+    return heroesWithAvatar;
 }
 
 export default exercise;
