00a5b5024aa8c8eb4fe2c6113e23e643d72c0d79
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 16:58:54 +0800

    reducing by forEach

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,17 @@
-function exercise (console) {
+/*
+ Exercise 15: Reducing Array
+    Sometime we need to perform an operation on more than one item at the same time, like get greatest value.
+    Filter cannot be used because it only check one item at a time.
+ */
+function exercise (heroesWithDeposit) {
+    let nameOfTheRichest;
 
+    /*
+     Add your code here
+        Try to use forEach to find the richest superhero's name.
+        So give me 'Batman'
+     */
+    return nameOfTheRichest;
 }
 
 export default exercise;
