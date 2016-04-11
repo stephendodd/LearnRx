23e885e60e175f18ee0e8b668c96cdb65ad52f59
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 15:11:47 +0800

    add contactMap to prepare

--- src/prepare.js
+++ src/prepare.js
@@ -3,3 +3,7 @@ import _ from 'lodash'
 Array.prototype.contactAll = function() {
     return _.flatten(this);
 };
+
+Array.prototype.contactMap = function(projectionFunctionReturnsArray) {
+    return _.flatMap(this, projectionFunctionReturnsArray);
+};
