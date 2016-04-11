862f267689562dee16c6ad5cefb0eb0804b600a1
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 17:50:34 +0800

    reduce function

--- src/data.js
+++ src/data.js
@@ -1,1 +1,15 @@
-export default [];
+const heroesWithDeposit = [{
+    name: 'Superman',
+    deposit: 100000
+}, {
+    name: 'Spidermam',
+    deposit: 1500
+}, {
+    name: 'Batman',
+    deposit: 999999999999
+}, {
+    name: 'Ironman',
+    deposit: 99999999999
+}];
+
+export default [heroesWithDeposit];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,14 @@
-function exercise (console) {
+/*
+ Exercise 17: Use reduce function
+    Try to get the richest superhero in array.
+ */
+function exercise (heroesWithDeposit) {
+    let richestInArray;
 
+    /*
+     Add your code here
+     */
+    return richestInArray;
 }
 
 export default exercise;
