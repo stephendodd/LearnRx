2379618bf52e300a070cd11d79bb9cdd45966a7e
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 17:47:19 +0800

    implement reduce

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,10 @@
 import _ from 'lodash'
 
 function postCheck() {
+    let result1 = [1,2,3,4].reduce((total, current) => {return total + current;});
+    let result2 = [1,2,3,4].reduce((total, current) => {return total + current;}, 5);
 
+    return `[1,2,3,4].reduce((total, current) => {return total + current;}) is [${result1}]\n[1,2,3,4].reduce((total, current) => {return total + current;}, 5) is [${result2}]`;
 }
 
 export default postCheck;
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,22 @@
-function exercise (console) {
+/*
+ Implement reduce function.
+     This reduce is different from the reduce in *ES5*, it returns an ARRAY.
+     This is because some chaining requirement in the future.
 
+     Example1: [1,2,3,4].reduce((total, current) => {return total + current;}) is [10]
+     Example2: [1,2,3,4].reduce((total, current) => {return total + current;}, 5) is [15]
+ */
+function exercise () {
+    Array.prototype.reduce = function(combinator, initValue) {
+        let sourceArray = this,
+            result;
+
+        /*
+         Add your code here
+         */
+
+        return [result];
+    }
 }
 
 export default exercise;
