e65c772fe0292265c1c638ac66a8cb527067ad7d
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-12 17:45:05 +0800

    reduce instead of filter

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,17 @@
-function exercise (console) {
-
+/*
+ Exercise 18: Use reduce further!
+     Try to retrieve {name, identity, avatar} for every heroes.
+     We only want the avatar with smallest size.
+ */
+function exercise (superHeroes) {
+    let heroesWithAvatar;
+    /*
+     Add your code here.
+     This time we use reduce instead of filter.
+     Give me:
+     [{name:'Superman', identity: 'Clerk Kent', avatar: 'http://super-heroes.com/superman/medium.jpg'}, ...]
+     */
+    return heroesWithAvatar;
 }
 
 export default exercise;
