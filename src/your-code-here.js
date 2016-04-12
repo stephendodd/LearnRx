0a8563456ef1c93adeee9abb9d9f229e3746a42a
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-12 17:58:45 +0800

    combine two array

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,18 @@
-function exercise (console) {
+/*
+ Exercise 19: combine 2 arrays (Zip).
+  It is very common to combine 2 arrays by progressively taking an item from each and combining the pair.
 
+  Think about zipper, each side is an array and each tooth is an item, that's how the zip operation works.
+ */
+function exercise(superHeroes, assistants) {
+    let heroesWithAssistants;
+    /*
+     Add your code here.
+      Zip the heroes and assistants together by heroId, ignore the hero who doesn't have an assistant.
+      Please try to give me:
+      [ {name: 'Batman', assistants: ['Alfred', 'Robin']}, ...]
+     */
+    return heroesWithAssistants;
 }
 
 export default exercise;
