4c57070ab1ed6f2ae575c379afa94238587b245d
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-13 10:49:53 +0800

    use zip

--- src/data.js
+++ src/data.js
@@ -1,1 +1,33 @@
-export default [];
+const superHeroes = [
+    {
+        id: 10002,
+        name: 'Batman',
+        identity: 'Bruce Wayne'
+    }, {
+        id: 20001,
+        name: 'Thor',
+        identity: 'Thor'
+    }, {
+        id: 20002,
+        name: 'Ironman',
+        identity: 'Tony Stark'
+    }, {
+        id: 20003,
+        name: 'Spiderman',
+        identity: 'Peter Parker'
+    }
+];
+
+const assistants = [
+    {
+        assistants: ['Alfred', 'Robin']
+    },
+    {
+        assistants: ['Heimdall']
+    },
+    {
+        assistants: ['J.A.R.V.I.S']
+    }
+];
+
+export default [superHeroes, assistants];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,14 @@
-function exercise (console) {
-
+/*
+ Exercise 21: Use zip function
+ */
+function exercise (superHeroes, assistants) {
+    let heroesWithAssistants;
+    /*
+     Add your code here.
+     Zip the heroes and assistants together by heroId.
+     Give me: [ {name: 'Batman', assistants: ['Alfred', 'Robin']}, ...]
+     */
+    return heroesWithAssistants;
 }
 
 export default exercise;
