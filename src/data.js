2899842c0ec89d27375871f7e27e2fdef330668c
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-13 16:18:55 +0800

    construct trees

--- src/data.js
+++ src/data.js
@@ -1,1 +1,28 @@
-export default [];
+const worlds = [
+    {id: 1, world: 'DC'},
+    {id: 2, world: 'MARVEL'}
+];
+const heroes = [
+    {worldId: 1, id: 10001, name: 'Superman', identity: 'Clerk Kent'},
+    {worldId: 1, id: 10002, name: 'Batman', identity: 'Bruce Wayne'},
+    {worldId: 2, id: 20001, name: 'Thor', identity: 'Thor'},
+    {worldId: 2, id: 20002, name: 'Ironman', identity: 'Tony Stark'}
+];
+const superPowers = [
+    {power: 'Flight', heroes: [10001, 20001, 20002]},
+    {power: 'Rich', heroes: [10002, 20002]},
+    {power: 'Strength', heroes: [10001, 20001]},
+    {power: 'Espionage', heroes: [10002]}
+];
+const avatars = [
+    {heroId: 10001, w: 100, h: 100, url: 'http://super-heroes.com/superman/medium.jpg'},
+    {heroId: 10001, w: 150, h: 150, url: 'http://super-heroes.com/superman/large.jpg'},
+    {heroId: 10002, w: 100, h: 100, url: 'http://super-heroes.com/batman/medium.jpg'},
+    {heroId: 10002, w: 150, h: 150, url: 'http://super-heroes.com/batman/large.jpg'},
+    {heroId: 20001, w: 100, h: 100, url: 'http://super-heroes.com/thor/medium.jpg'},
+    {heroId: 20001, w: 150, h: 150, url: 'http://super-heroes.com/thor/large.jpg'},
+    {heroId: 20002, w: 100, h: 100, url: 'http://super-heroes.com/ironman/medium.jpg'},
+    {heroId: 20002, w: 150, h: 150, url: 'http://super-heroes.com/ironman/large.jpg'}
+];
+
+export default [worlds, heroes, superPowers, avatars];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,14 @@
-function exercise (console) {
-
+/*
+ Exercise 23: Construct a tree from Arrays
+ */
+function exercise (worlds, heroes, superPowers, avatars) {
+    let heroesInTheWorld;
+    /*
+     Add your code here,
+      Try to construct an array with item like:
+      [{world: 'DC', heroes: [{id, name, identity, superPower, avatars}...]}...]
+     */
+    return heroesInTheWorld;
 }
 
 export default exercise;
