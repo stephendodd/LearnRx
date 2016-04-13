ae3b627d89de23860391a2175cb3770d0239fe1d
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-13 10:08:35 +0800

    implement zip

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
 
 function postCheck() {
-
+    const afterZip = Array.zip([1,2], [3,4], (l, r) => {return l + r;});
+    return `Array.zip([1,2], [3,4], (l, r) => {return l + r;}) is [${afterZip}]`;
 }
 
 export default postCheck;
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,21 @@
-function exercise (console) {
+/*
+ Exercise 20: Implement zip
+    Let's add a static zip function to Array, it accept 2 arrays and a combiner.
+    It traverses each array at same time, and call combiner on both left-side-item and right-side-item,
+    because it require 2 items from left and right, so the array returned by zip will only be as large as the smallest array.
 
+    Example: Array.zip([1,2], [3,4], (l,r) => { return l + r;}) is [4,6]
+ */
+function exercise () {
+    Array.zip = function(leftArray, rightArray, combiner) {
+        let results = [];
+
+        /*
+         Add your code here
+         */
+
+        return results;
+    }
 }
 
 export default exercise;
