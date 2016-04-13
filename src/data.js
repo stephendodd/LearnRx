f7bbbad1fd1150a711a6f9c7aa51eab27103ec4b
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-13 15:18:54 +0800

    mixed practice

--- src/data.js
+++ src/data.js
@@ -1,1 +1,67 @@
-export default [];
+const superHeroes = [
+    {
+        world: 'DC',
+        heroes: [
+            {
+                id: 10001,
+                name: 'Superman',
+                identity: 'Clerk Kent',
+                superPower: ['Flight', 'Strength', 'X-Ray vision'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/superman/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/superman/large.jpg'}
+                ],
+                appearances: [
+                    {appearance: 'first', time: '1938', place: 'Action Comics #1'},
+                    {appearance: 'last', time: '2016', place: 'Batman v Superman: Dawn of Justice'}
+                ]
+            }, {
+                id: 10002,
+                name: 'Batman',
+                identity: 'Bruce Wayne',
+                superPower: ['Rich', 'Martial arts', 'Espionage'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/batman/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/batman/large.jpg'}
+                ],
+                appearances: [
+                    {appearance: 'first', time: '1939', place: 'Detective Comics #27'},
+                    {appearance: 'last', time: '2016', place: 'Batman v Superman: Dawn of Justice'}
+                ]
+            }
+        ]
+    }, {
+        world: 'MARVEL',
+        heroes: [
+            {
+                id: 20001,
+                name: 'Thor',
+                identity: 'Thor',
+                superPower: ['Flight', 'Strength', 'Lightning'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/thor/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/thor/large.jpg'}
+                ],
+                appearances: [
+                    {appearance: 'first', time: '1962', place: 'Journey into Mystery #83'},
+                    {appearance: 'last', time: '2016', place: 'Captain America: Civil War'}
+                ]
+            }, {
+                id: 20002,
+                name: 'Ironman',
+                identity: 'Tony Stark',
+                superPower: ['Rich', 'Flight', 'Missile', 'Laser'],
+                avatars: [
+                    {w: 100, h: 100, url: 'http://super-heroes.com/ironman/medium.jpg'},
+                    {w: 150, h: 150, url: 'http://super-heroes.com/ironman/large.jpg'}
+                ],
+                appearances: [
+                    {appearance: 'first', time: '1963', place: 'Tales of Suspense #39'},
+                    {appearance: 'last', time: '2016', place: 'Captain America: Civil War'}
+                ]
+            }
+        ]
+    }
+];
+
+export default [superHeroes];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,19 @@
-function exercise (console) {
+/*
+ Exercise 22: mixed practice
+    Try to play with contactMap, filter, reduce, zip
+ */
+function exercise (superHeroes) {
+    let heroesWithAvatarAndFirstAppearance;
 
+    /*
+     Add your code here.
+      Returns an {name, identity, avatar, firstAppearances} object for each hero.
+      The avatar should choose the smallest and the firstAppearances should select the place
+
+      Example:
+      [{name:'Superman', identity:'Clerk Kent', avatar: 'http://super-heroes.com/superman/medium.jpg', firstAppearance: 'Action Comics #1'}...]
+     */
+    return heroesWithAvatarAndFirstAppearance;
 }
 
 export default exercise;
