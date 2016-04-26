413b47d307688def0dddac02025653d26a3ee286
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-26 17:40:17 +0800

    reactive runner

--- src/runner.js
+++ src/runner.js
@@ -1,24 +1,32 @@
 import _ from 'lodash'
 import 'babel-polyfill'
+import Rx from 'rx'
 import trackableConsole from './trackable-console'
 import exercise from './your-code-here'
 import postCheck from './post-check'
 import './prepare'
 import expectation from './expectation'
-import data from './data'
+import reactiveData from './reactive-data'
 
 try {
-    let args = _.concat(data, trackableConsole);
-    let actual = exercise(...args) || trackableConsole.records || postCheck() || null;
+    let args = _.concat(reactiveData, trackableConsole);
 
-    if(_.isEqual(actual, expectation)) {
-        console.log('Great! You have beaten this exercise!');
-    } else {
-        console.log('Expected:');
-        console.log('\t', JSON.stringify(expectation));
-        console.log('Actual:');
-        console.log('\t', JSON.stringify(actual));
-    }
+    let actual = exercise(...args);
+    Rx.Observable.merge(...reactiveData).subscribeOnCompleted(() => {
+        actual = actual
+            || trackableConsole.records
+            || postCheck()
+            || null;
+
+        if(_.isEqual(actual, expectation)) {
+            console.log('Great! You have beaten this exercise!');
+        } else {
+            console.log('Expected:');
+            console.log('\t', JSON.stringify(expectation));
+            console.log('Actual:');
+            console.log('\t', JSON.stringify(actual));
+        }
+    });
 } catch(e) {
     console.log('ERROR: ', e.message);
 }
