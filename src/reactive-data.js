62573f1bbc32b7292ade388047d4d3b2db40e21e
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-26 23:53:23 +0800

    consistent interval and filter out empty item from stream

--- src/reactive-data.js
+++ src/reactive-data.js
@@ -1,12 +1,12 @@
 import Rx from 'rx'
 import data from './data'
 
+const dueTime = parseInt(Math.random() * 1000);
+const interval = parseInt(Math.random() * 500);
 const reactiveData = data.map(d => {
-    const dueTime = parseInt(Math.random() * 1000);
-    const interval = parseInt(Math.random() * 500);
     const timer = Rx.Observable.timer(dueTime, interval).take(d.length);
     const rData = Rx.Observable.from(d);
-    return Rx.Observable.zip(timer, rData, (t, rd) => rd);
+    return Rx.Observable.zip(timer, rData, (t, rd) => rd).filter(rd => rd);
 });
 
 export default reactiveData;
