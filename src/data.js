0f827d80ae99972dcb15185cc103b883192036bf
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-26 23:52:06 +0800

    takeUntil

--- src/data.js
+++ src/data.js
@@ -1,1 +1,34 @@
-export default [];
+const superVillains = [
+    {name: 'Joker', hero: 'Batman'},
+    {name: 'Sinestro', hero: 'Lantern'},
+    {name: 'General Zod', hero: 'Superman'},
+    {name: 'Loki', hero: 'Thor'},
+    {name: 'Mandarin', hero: 'Ironman'},
+    {name: 'Green Goblin', hero: 'Spiderman'},
+    {name: 'Catwoman', hero: 'Batman'},
+    {name: 'Doctor Octopus', hero: 'Spiderman'},
+    {name: 'Iron Monger', hero: 'Ironman'},
+    {name: 'Lex Luthor', hero: 'Superman'},
+    {name: 'Parallax', hero: 'Lantern'},
+    {name: 'Bizarro', hero: 'Superman'},
+    {name: 'Mr. Freeze', hero: 'Batman'},
+    {name: 'Absorbing Man', hero: 'Thor'},
+    {name: 'Zoom', hero: 'Flash'},
+    {name: 'Penguin', hero: 'Batman'},
+    {name: 'Captain Cold', hero: 'Flash'},
+    {name: 'Sandman', hero: 'Spiderman'}
+];
+
+const batSuitMaintaining = [
+    ,
+    ,
+    ,
+    ,
+    {event: 'Smile'},
+    ,
+    ,
+    {event: 'Fix'},
+    ,
+];
+
+export default [superVillains, batSuitMaintaining];
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,20 @@
-function exercise (console) {
+/*
+ Exercise 27: Unsubscribe when another event fires.
+  Batman is fighting with his enemies from all super villains.
+  Of course, this time Batman has to fix his BatSuit. But when it need to be fixed, is an event.
 
+  For reference:
+   [Villain,,,,,,Villain,,,,Villain,,,,,,,,.Villain,,,,]
+   [,,,,,,,,,Smile,,,,,,,,Fix,,,,]
+   === is ===
+   [Villain,,,,,,Villain]
+ */
+function exercise (superVillains, batSuitMaintaining, console) {
+    /*
+     Add your code here.
+     Try to use '.takeUntil(stream)'.
+     takeUntil function completes a stream when other event occurs.
+     */
 }
 
 export default exercise;
