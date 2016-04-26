2f688987eebc98d4649ec62703257ca9d055f433
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-26 18:22:16 +0800

    first rx quest

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,4 +1,10 @@
-function exercise (console) {
+/*
+ Exercise 24: Filtering
+ Super Villain is jumping out one by one from now, Batman is going to beat down all his enemies.
+ Print the name of batman's villains.
+ THIS IS NOT A TRICK QUESTION, IT'S AS EASY AS IT SEEMS.
+ */
+function exercise (villains, console) {
 
 }
 
