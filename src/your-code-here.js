fff7c1596eaf6a6ace9934ec054a7493205aa2d9
Stephen Dodd <stephen.dodd@rea-group.com>
2016-04-28 14:50:19 +1000

    Working on combing the forEach and map functions

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -2,10 +2,25 @@
  Exercise 11: Use map and contactAll to project and flatten then heroes from 2 worlds to name array
  */
 function exercise (superHeroes) {
-    let allHeroNames;
+    let allHeroNames = [];
     /*
      Add your code here
      */
+
+     allHeroNames = superHeroes.forEach(heroWorld => {
+       //console.log(heroWorld);
+       heroWorld.heroes.forEach(hero => {
+         return allHeroNames.push(hero);
+         //console.log(hero);
+       });
+     }).map( hero => {
+       return hero.name;
+     });
+
+     /*allHeroNames = allHeroNames.map(hero => {
+       return hero.name;
+     });*/
+
     return allHeroNames;
 }
 
