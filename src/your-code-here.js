651ab249d3766b680738482ea726ddf093148723
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-12 23:16:24 +1000

    Finish exercise 12

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -6,12 +6,87 @@
  */
 function exercise (superHeroes) {
     let heroesWithAvatar;
+    let heroesWithAvatars;
+    let heroesWithAvatar2
     /*
      Add your code here, use one or more contactAll, map, filter to project on the tree.
      Try to give me heroes like this:
      [{name:'Superman', identity: 'Clerk Kent', avatar: 'http://super-heroes.com/superman/medium.jpg'}, ...]
      */
-    return heroesWithAvatar;
+     heroesWithAvatar = superHeroes.map(world => {
+       return world.heroes
+     }).contactAll().map(hero => {
+       return {
+         "name": hero.name,
+         "identity": hero.identity,
+         "avatars": hero.avatars.contactAll().filter(avatar => {
+           return avatar.w == 100;
+         })
+       }
+     })
+
+
+
+    //  heroesWithAvatars = superHeroes.map(world => {
+    //    return world.heroes
+    //  }).contactAll().map(hero => {
+    //    return {
+    //      hero.avatars
+    //    }
+    //  }).contactAll().filter(avatar => {
+    //    return avatar.w == 100;
+    //  })
+
+     heroesWithAvatar2 = superHeroes.map(world => {
+       return world.heroes;
+     }).contactAll().map(hero => {
+       return hero.avatars.contactAll().filter(avatar => {
+         return avatar.w == 100
+       }).map(avatar => {
+         return {
+           "name": hero.name,
+           "identity": hero.identity,
+           "avatar": avatar.url
+         }
+       })
+     }).contactAll()
+
+    //  heroesWithAvatar2 = heroesWithAvatar2.map(hero => {
+    //    return hero.avatars.contactAll().filter(avatar => {
+    //      return avatar.w == 100
+    //    }).map(avatar => {
+    //      return {
+    //        "name": hero.name,
+    //        "identity": hero.identity,
+    //        "avatar": avatar.url
+    //      }
+    //    })
+    //  }).contactAll()
+
+     console.log(heroesWithAvatar2);
+
+
+
+
+      //  return hero.avatars.filter(avatar => {
+      //    console.log("avatar",avatar.w);
+      //    return avatar.w == 100
+      //  })
+
+
+    //    .map(hero => {
+    //    return {
+    //      "name": hero.name
+    //    }
+    //  })
+      //  return hero.avatars.filter(avatar => {
+      //    avatar.w == 100
+      //  })
+
+
+
+    return heroesWithAvatar2;
 }
+
 
 export default exercise;
